93ceb626dc1f80b5b5d1e29273f2c7f3cd966c6a
Rob Jewell <jewell.robertp@gmail.com>
2014-04-11 14:58:57 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grassroots::Application.config.secret_key_base = 'f580ea47d18c86d4e584ee64e48a40ab5e400e1d52e9b92e3490bd1ea32e8a25be6aa2df08b2d439e46e37d4e08b836d36ca3be6a3ad4e8d323b317537eee203'
