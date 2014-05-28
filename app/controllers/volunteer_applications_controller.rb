6f59ba9717ba7d86a1f3bd16ede845f14b9f7b85
Rob Jewell <jewell.robertp@gmail.com>
2014-05-28 13:26:20 -0400

    refactors create action code in volunteer application controller

--- app/controllers/volunteer_applications_controller.rb
+++ app/controllers/volunteer_applications_controller.rb
@@ -6,13 +6,8 @@ class VolunteerApplicationsController < ApplicationController
   end
 
   def create
-    conversation1_about_volunteer_application = Conversation.create
-    message = PrivateMessage.create(message_params)
-    message.update_columns(conversation_id: conversation1_about_volunteer_application.id)
-    volunteer_application = VolunteerApplication.create(user_id: current_user.id, project_id: params[:project_id])
-    conversation1_about_volunteer_application.update_columns(volunteer_application_id: volunteer_application.id)
-    project_administrator = User.find(message.recipient_id)
-    project_administrator.conversations << conversation1_about_volunteer_application
+    @volunteer_application = VolunteerApplication.create(user_id: current_user.id, project_id: params[:project_id])
+    sends_application_to_project_administrator(@volunteer_application)
     redirect_to conversations_path
   end
 
@@ -20,5 +15,18 @@ private
 
   def message_params
     params.permit(:subject, :sender_id, :recipient_id, :body)
+  end
+
+  def sends_application_to_project_administrator(application)
+    conversation1_about_volunteer_application = Conversation.create
+    message = PrivateMessage.create(message_params)
+    message.update_columns(conversation_id: conversation1_about_volunteer_application.id)
+    project_administrator = User.find(message.recipient_id)
+    project_administrator.conversations << conversation1_about_volunteer_application
+    conversation1_about_volunteer_application.update_columns(volunteer_application_id: application.id)
+  end
+
+  def creates_volunteer_application
+    volunteer_application = VolunteerApplication.create(user_id: current_user.id, project_id: params[:project_id])
   end
 end
