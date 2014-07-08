99213dc2ded2338b80d2151043fef2e7d4630bfd
Rob Jewell <jewell.robertp@gmail.com>
2014-07-08 12:11:09 -0400

    adds relationship method for getting the name of an organization from a leader

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,8 @@ class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: 'User'
   belongs_to :leader, class_name: 'User'
   has_many :newsfeed_items, as: :newsfeedable
+
+  def leader_organization_name
+    leader.organization.name
+  end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -4,4 +4,16 @@ describe Relationship do
   it { should have_many(:newsfeed_items)}
   it { should belong_to(:follower)}
   it { should belong_to(:leader)}
+
+  describe "#leader_organization_name" do
+    it "shows the leader's organization name" do
+      alice = Fabricate(:user, user_group: "nonprofit")
+      huggey_bear = Fabricate(:organization)
+      bob = Fabricate(:user, user_group: "nonprofit", organization_id: huggey_bear.id)
+      alice.follow!(bob)
+      first_relationship = Relationship.first
+
+      expect(first_relationship.leader_organization_name).to eq(huggey_bear.name)
+    end
+  end
 end
