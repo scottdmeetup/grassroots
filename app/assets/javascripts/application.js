4c03a812775779b7f01357ac5a53ab2159a3195f
Rob Jewell <jewell.robertp@gmail.com>
2014-07-09 10:30:58 -0400

    tweaking application.js to precompile assets in heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//= require jquery-ui
 //= require bootstrap
 //= require jquery.ui.widget
 //= require jquery.iframe-transport
